b37beb81278bcf5ac643dddce0a04610465cb731
Developer-Hannya <42482469+Developer-Hannya@users.noreply.github.com>
2025-07-16 16:38:14 -0300

    adicionando exemplo 01 e saída

---
+++ tutorial 01 exemplo.py
@@ -0,0 +1,22 @@
+import os, sys
+project_directory = os.getcwd()
+sys.path.append(project_directory)
+
+from bciflow.modules.core.kfold import kfold
+from bciflow.modules.tf.bandpass.chebyshevII import chebyshevII
+from bciflow.modules.fe import logpower
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as lda
+
+# tutorial 01
+
+from bciflow.datasets.cbcic import cbcic
+
+dataset = cbcic(subject=1, path='data/cbcic/')
+
+print("EEG signals shape:", dataset["X"].shape)
+print("Labels:", dataset["y"])
+print("Class dictionary:", dataset["y_dict"])
+print("Events:", dataset["events"])
+print("Channel names:", dataset["ch_names"])
+print("Sampling frequency (Hz):", dataset["sfreq"])
+print("Start time (s):", dataset["tmin"])

ba5203283e0b7673c38055d667d30076e32c9961
Developer-Hannya <42482469+Developer-Hannya@users.noreply.github.com>
2025-07-31 07:34:31 -0300

    Add files via upload

---
+++ tutorial 02 pipeline.py
@@ -0,0 +1,36 @@
+from bciflow.datasets.cbcic import cbcic
+from bciflow.modules.core.kfold import kfold
+from bciflow.modules.tf.filterbank import filterbank
+from bciflow.modules.sf.csp import csp
+from bciflow.modules.fe.logpower import logpower
+from bciflow.modules.fs.mibif import MIBIF
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as lda
+import pandas as pd
+from bciflow.modules.analysis.metric_functions import accuracy #não está funcionando
+
+dataset = cbcic(subject=1, path='data/cbcic/')
+
+pre_folding = {'tf': {filterbank, {'kind_bp': 'chebyshevII'}}}
+
+sf = csp()
+fe = logpower
+fs = MIBIF(8, clf=lda())
+clf = lda()
+
+pos_folding = {
+    'sf': {sf, {}},
+    'fe': {fe, {}},
+    'fs': {fs, {}},
+    'clf': {clf, {}}
+}
+
+results = kfold(
+    target=dataset,
+    start_window=dataset['events']['cue'][0] + 0.5,
+    pre_folding=pre_folding,
+    pos_folding=pos_folding
+)
+
+df = pd.DataFrame(results)
+acc = accuracy(df)
+print(f"Accuracy: {acc:.4f}")
